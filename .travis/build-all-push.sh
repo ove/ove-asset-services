d7f8aa98621112b7cd02a85abb71f21b3e1f380a
Ovidiu Șerban <oserban@users.noreply.github.com>
2018-12-05 14:53:57 +0000

    Found out that the build task for the archive service was missing (#68)
    * Revert Route Routing
    
    * Added archive service

--- .travis/build-all-push.sh
+++ .travis/build-all-push.sh
@@ -5,4 +5,5 @@ scriptPath=$(dirname "$(readlink -f "$0")")
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 ${scriptPath}/../packages/ove-asset-manager/build.sh --push && \
-${scriptPath}/../packages/ove-service-imagetiles/build.sh --push
+${scriptPath}/../packages/ove-service-imagetiles/build.sh --push && \
+${scriptPath}/../packages/ove-service-archives/build.sh --push
--- .travis/build-all.sh
+++ .travis/build-all.sh
@@ -3,4 +3,5 @@
 scriptPath=$(dirname "$(readlink -f "$0")")
 
 ${scriptPath}/../packages/ove-asset-manager/build.sh && \
-${scriptPath}/../packages/ove-service-imagetiles/build.sh
+${scriptPath}/../packages/ove-service-imagetiles/build.sh && \
+${scriptPath}/../packages/ove-service-archives/build.sh
